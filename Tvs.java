5dfe685399aad816f80576b6ecaa56cb141429fd
sachicv2 <113331204+sachicv2@users.noreply.github.com>
2022-09-29 21:29:41 +0530

    Add files via upload

---
+++ Tvs.java
@@ -0,0 +1,18 @@
+package com.xworkz.boot;
+
+public class Tvs {
+
+	public static void main(String[] args) {
+	int x=12;
+	int y=35;
+	int z=355;//swapping number with extra variable
+	int temp=x;
+	x=y;
+	y=z;
+	z=temp;
+	System.out.println(x);
+	System.out.println(y);
+	System.out.println(z);
+	}
+
+}
